c4c6407a342672b28db2387923b85c3a8c515ef8
Felipe Alexandre <fealecasbor@gmail.com>
2025-02-25 16:32:25 -0300

    Aula de PHP com exemplos de concatenação de variaveis

---
+++ aula 01/ola.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <body>
+ 
+        <?php
+          
+            $nome = "Felipe";
+            $sobrenome = "Alexandre";
+            $numero = 8;
+
+            echo "${nome} <h1> ${sobrenome} </h1>";
+            echo $numero . " <p> " . $nome . " " . $sobrenome . "</p>";
+        ?>
+
+    </body>
+</html>
